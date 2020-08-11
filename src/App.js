036f6b553b81d0724178b047a5aeeca6e1e7b0fe
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-08-11 13:19:50 -0500

    Add main ApolloClient and PokemonContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonContainer } from './containers/PokemonContainer';
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh',
+  });
+
   return (
-    <div className="App">
-      Charizard React App
-    </div>
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
   );
 }
 
